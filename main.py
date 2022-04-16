1d47eb1706b79c1b61cce0ced271a2585414c95e
nankuF <fireloki87@gmail.com>
2022-04-16 09:14:30 +0300

    added main()

--- main.py
+++ main.py
@@ -7,32 +7,38 @@ import pandas as pd
 from dotenv import load_dotenv
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-load_dotenv()
-path_to_excel = os.getenv('PATH_TO_EXCEL')
 
-try:
-    products = pd.read_excel(path_to_excel, na_values=None, keep_default_na=False).to_dict(orient='records')
-except ValueError:
-    raise ValueError('Файл должен быть в формате Excel')
+def main():
+    load_dotenv()
+    path_to_excel = os.getenv('PATH_TO_EXCEL')
 
-grouped_products = collections.defaultdict(list)
-for product in products:
-    grouped_products[product['Категория']].append(product)
+    try:
+        products = pd.read_excel(path_to_excel, na_values=None, keep_default_na=False).to_dict(orient='records')
+    except ValueError:
+        raise ValueError('Файл должен быть в формате Excel')
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
+    grouped_products = collections.defaultdict(list)
+    for product in products:
+        grouped_products[product['Категория']].append(product)
 
-template = env.get_template('template.html')
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
 
-date_create_company = 1920
-age_company = (datetime.datetime.now().year - date_create_company)
+    template = env.get_template('template.html')
 
-rendered_page = template.render(goods=grouped_products, age_company=age_company)
+    date_create_company = 1920
+    age_company = (datetime.datetime.now().year - date_create_company)
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    rendered_page = template.render(goods=grouped_products, age_company=age_company)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
