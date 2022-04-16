577dcbc70b9519b744ca9952aa0764f919b82156
nankuF <fireloki87@gmail.com>
2022-04-16 08:42:20 +0300

    added path to file in enviroments

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import collections
 import datetime
+import os
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 import pandas as pd
+from dotenv import load_dotenv
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-products = pd.read_excel('wine3.xlsx', na_values=None, keep_default_na=False).to_dict(orient='records')
+load_dotenv()
+path_to_excel = os.getenv('PATH_TO_EXCEL')
+
+try:
+    products = pd.read_excel(path_to_excel, na_values=None, keep_default_na=False).to_dict(orient='records')
+except ValueError:
+    raise ValueError('Файл должен быть в формате Excel')
 
 grouped_products = collections.defaultdict(list)
 for product in products:
